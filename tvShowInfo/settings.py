d1bb3c5b402c39b44409ce7fb893b05071884bbe
Muhammad Asyraf <muhammad.asyraf@ui.ac.id>
2023-03-12 15:26:55 +0700

    add docker

--- tvShowInfo/settings.py
+++ tvShowInfo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,10 +85,10 @@ WSGI_APPLICATION = "tvShowInfo.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tvshow',
-        'USER': 'postgres',
-        'PASSWORD': 'asraf290302',
-        'HOST': 'localhost',
+        'NAME': os.getenv("POSTGRES_NAME", "tvshow"),
+        'USER': os.getenv("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "asraf290302"),
+        'HOST': os.getenv("POSTGRES_HOST", "localhost"),
         'PORT': '5432',
   }
 }
